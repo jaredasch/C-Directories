d8e927b1e8795fba7cfc05de35e11735d63879cb
jaredasch1 <jaredasch1@gmail.com>
2018-11-04 14:10:13 -0500

    better input

--- dir_info.c
+++ dir_info.c
@@ -9,6 +9,10 @@
 int dir_size(char *path, char RECURSIVE){
     int size = 0;
     DIR *d = opendir(path);
+    if(!d){
+        printf("Invald directory %s\n", path);
+        return -1;
+    }
     struct dirent *f = readdir(d);
     while(f){
         char *path_cpy = malloc(strlen(path) + strlen(f->d_name) + 10);
@@ -38,7 +42,11 @@ void list_dir(char *path, int depth){
     struct dirent *f = readdir(d);
     while(f){
         char *path_cpy = malloc(strlen(path) + strlen(f->d_name) + 10);
-        strcat(strcpy(path_cpy, path), f->d_name);
+        strcpy(path_cpy, path);
+        if(path_cpy[strlen(path_cpy)-1] != '/'){
+            strcat(path_cpy, "/");
+        }
+        strcat(path_cpy, f->d_name);
         struct stat *s = malloc(sizeof(struct stat));
         stat(path_cpy, s);
 
@@ -53,7 +61,7 @@ void list_dir(char *path, int depth){
         printf("%c%s %10lld B %.*s %-60s\n", f->d_type == DT_DIR ? 'd' : '-', perms,  s->st_size, depth, "\t\t\t\t\t\t\t\t\t", path_cpy);
         free(perms);
         if(f->d_type == DT_DIR && strcmp(".", f->d_name) && strcmp("..", f->d_name)){
-            list_dir(strcat(path_cpy, "/"), depth+1);
+            list_dir(path_cpy, depth+1);
         }
         free(path_cpy);
         free(s);
@@ -63,17 +71,18 @@ void list_dir(char *path, int depth){
 }
 
 int main(int argc, char *argv[]){
-  // printf("statistics for directory: . \n");
-  char* dir = malloc(100);
-  if (argc <= 1){
-      printf("Enter a directory to look into: ");
-      fgets(dir, 100, stdin);
-      dir[strlen(dir)-1] = 0; // Set \n to NULL
-  } else {
-      dir = argv[1];
-  }
-  printf("Total directory size (non-recursive): %d \n", dir_size(dir, 0));
-  // printf("Total directory size (recursive): %d \n", dir_size(dir, 1));
-  list_dir(dir, 0);
-  return 0;
+    // printf("statistics for directory: . \n");
+    char* dir = malloc(100);
+    if (argc <= 1){
+        printf("Enter a directory to look into: ");
+        fgets(dir, 100, stdin);
+        dir[strlen(dir)-1] = 0; // Set \n to NULL
+    } else {
+        dir = argv[1];
+    }
+
+    printf("Total directory size (non-recursive): %d \n", dir_size(dir, 0));
+    printf("Total directory size (recursive): %d \n", dir_size(dir, 1));
+    list_dir(dir, 0);
+    return 0;
 }
